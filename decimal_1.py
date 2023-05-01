455d925437e9941b4ebdc4d7ca343733fe3e31a1
mianmiantea2019 <sguo321@gatech.edu>
2023-05-01 12:28:31 -0400

    decimal_1

---
+++ decimal_1.py
@@ -0,0 +1,11 @@
+# convert a decimal number to octal format, you can use the oct() function.
+print(oct(47))
+
+#  convert a decimal number to binary format, you can use the bin() function.
+print(bin(47))
+
+#  convert a decimal number to hexadecimal format, you can use the hex() function
+print(hex(47))
+
+
+
